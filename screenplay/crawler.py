3989f0264607382371e9ffe638be55a851fa3e20
Eric Prykhodko <eprykhodko@bcdtriptech.com>
2019-01-21 23:54:26 +0200

    modernize asyncio syntax

--- screenplay/crawler.py
+++ screenplay/crawler.py
@@ -15,14 +15,11 @@ async def crawl(show):
     async with ClientSession() as session:
         page_text = await fetch_page(INDEX_PAGE + show, session)
         tasks = [
-            asyncio.create_task(
-                fetch_page(BASE_PATH + item.get("href"), session)
-            )
+            fetch_page(BASE_PATH + item.get("href"), session)
             for item in html.fromstring(page_text).xpath(SEASON_EPISODES_XPATH)
         ]
         return await asyncio.gather(*tasks)
 
 
 def gather_pages(show):
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(crawl(show))
+    return asyncio.run(crawl(show))
